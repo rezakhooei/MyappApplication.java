af0dd5342b1d071b0ccc833408adee10abeaa2aa
newsroom <reza_khooey@yahoo.com>
2024-01-08 21:10:07 +0330

    last changed uploaded in server and worked 14021018-2100

--- src/main/java/com/tdxir/myapp/service/RecordAndProccessMessageService.java
+++ src/main/java/com/tdxir/myapp/service/RecordAndProccessMessageService.java
@@ -119,15 +119,15 @@ public class RecordAndProccessMessageService {
 
             File filereply = new File(targetLocation.toString());
 
-            if(MyappApplication.WinLinux==1) {
+          //  if(MyappApplication.WinLinux==1) {
 
                inf1="قیمت رژ چنده؟";
-            }
-            else {
+           // }
+          /*  else {
 
                   String transcription=service.createTranscription(request,filereply).getText();//.createTranscription((request,file).getText();
                    inf1= transcription;
-            }
+            }*/
 
 
             System.out.println(inf1);
