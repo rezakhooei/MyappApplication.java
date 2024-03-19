c7276f7474e4de10db90a65f6ee206d81a2d0657
newsroom <reza_khooey@yahoo.com>
2024-03-19 14:32:13 +0330

    14021229-1421

--- src/main/java/com/tdxir/myapp/controller/ReciveMessageController.java
+++ src/main/java/com/tdxir/myapp/controller/ReciveMessageController.java
@@ -64,8 +64,8 @@ public class ReciveMessageController {
      {   //googleSpeech.initialize();
          String checkBox1="false",checkBox2="false",checkBox3="false",checkBox4="false",panel1="",panel2="";
 
-         inf2="Rds="+selected_rds+"-"+selected_chks+"V="+fileVoice.toString()+"I="+fileImage.toString();
-         inf1=inf2;
+         inf2="Rds="+selected_rds+"-Chks="+selected_chks;
+        // inf1=inf2;
          String[] panels = selected_rds.split(",");
          String[] panel11 = panels[0].split("-");
          String[] panel22 = panels[1].split("-");
