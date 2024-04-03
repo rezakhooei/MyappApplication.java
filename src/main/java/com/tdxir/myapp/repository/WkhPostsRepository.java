d93f2b965dfff7440651e5761fc1fa60ed2de189
newsroom <reza_khooey@yahoo.com>
2024-04-04 01:18:35 +0330

    14030116-0118

--- src/main/java/com/tdxir/myapp/repository/WkhPostsRepository.java
+++ src/main/java/com/tdxir/myapp/repository/WkhPostsRepository.java
@@ -19,23 +19,24 @@ public interface WkhPostsRepository extends JpaRepository<WkhPosts, Long> {
 
     public List<String> PostIdName (@Param("productName") String productName);
     // /var/www/tdx.ir/public_html
-    @Query("SELECT  am.meta_value \n" +
+    @Query("SELECT\n" +
+            "\n" +
+            "    CONCAT( \"/var/www/tdx.ir/public_html/wp-content/uploads/\", am.meta_value) AS siteurl\n" +
             "FROM\n" +
-            "    WkhPosts  p\n" +
-            "LEFT JOIN\n" +
+            "    WkhPosts p\n" +
+            "        LEFT JOIN\n" +
             "    wkh_postmeta pm ON\n" +
-            "        pm.post_id = p.id AND\n" +
-            "        pm.meta_key = '_thumbnail_id'\n" +
-            "LEFT JOIN\n" +
+            "                pm.post_id = p.id AND\n" +
+            "                pm.meta_key = '_thumbnail_id'\n" +
+            "        LEFT JOIN\n" +
             "    wkh_postmeta am ON\n" +
-            "am.post_id  = 3977 AND \n"+//pm.meta_value  AND \n"+
-
-            "        am.meta_key = '_wp_attached_file'\n" +
+            "                CAST(am.post_id as char) = CAST(pm.meta_value as char) AND\n" +
+            "                am.meta_key = '_wp_attached_file'\n" +
             "WHERE\n" +
-            "    p.post_type = 'product'\n" +
-            "    AND p.post_status = 'publish'\n" +
-            "    AND am.meta_value IS NOT NULL\n" +
-            "and p.id=:id")
+            "        p.post_type = 'product'\n" +
+            "  AND p.post_status = 'publish'\n" +
+            "  AND am.meta_value IS NOT NULL\n" +
+            "  and p.id=:id")
     public String imageUrl(@Param("id") String id);
     @Modifying
     @Transactional                 //   _price , _sale_price  , _regular_price , wcwp_wholesale
