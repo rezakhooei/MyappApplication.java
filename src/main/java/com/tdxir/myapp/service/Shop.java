70093f60e7b2bce6b872805c19cd6f32b2e9199c
newsroom <reza_khooey@yahoo.com>
2024-04-21 11:40:19 +0330

    14030202-1001 regularprice,wholesale and saleprice updated

--- src/main/java/com/tdxir/myapp/service/Shop.java
+++ src/main/java/com/tdxir/myapp/service/Shop.java
@@ -602,7 +602,7 @@ public class Shop {
                 else if(role==ADMIN)
                 {   List<Long> buyPrice=wkhPostMetaRepository.buyPrice(message);
                     if(buyPrice.size()!=0)
-                    processList.add(price.get(0)+"(قیمت خرید"+String.valueOf(buyPrice.get(buyPrice.size()-1))+")");
+                    processList.add(price.get(0)+ "ریال"+"(قیمت خرید"+String.valueOf(buyPrice.get(buyPrice.size()-1))+")");
                     else processList.add(price.get(0)+"--"+"قیمت خرید ندارد");
                 }
             }
@@ -744,7 +744,7 @@ public class Shop {
             }
             else if(i==2) {
                 if(processList.get(i - 1)!="-1")
-                    jsonObject.put("inf_text", "قیمت : " + processList.get(i - 1) + "ریال");
+                    jsonObject.put("inf_text", "قیمت : " + processList.get(i - 1) );
                 else jsonObject.put("inf_text",  "قیمت تعریف نشده است");
 
             }
